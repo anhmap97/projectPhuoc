76c05eb667570ff35eac9819ff20b06478efde73
anhhuunguyen97 <anhhuunguyen97@gmail.com>
2021-05-17 11:23:17 +0700

    update

--- public/index.js
+++ public/index.js
@@ -2,48 +2,93 @@ const data = {
   bookingapp: {
     ios: null,
     android:
-      'https://play.google.com/store/apps/details?id=com.eplus.flybooking',
+      "https://play.google.com/store/apps/details?id=com.eplus.flybooking",
   },
-}
+  vemaybaybmt: {
+    ios: "https://apps.apple.com/us/app/travelaki/id1249951325",
+    android:
+      "https://play.google.com/store/apps/details?id=com.eplus.vemaybaybmt.flybooking",
+  },
+  agentvemaybaybmt: {
+    ios: null,
+    android:
+      "https://play.google.com/store/apps/details?id=vn.eplus.vemaybaybmt.flyagent",
+  },
+  hopdenoto: {
+    ios: null,
+    android: "https://play.google.com/store/apps/details?id=vn.eplus.blackbox",
+  },
+  smeplatform: {
+    ios: null,
+    android:
+      "https://play.google.com/store/apps/details?id=vn.eplus.smeplatform",
+  },
+  timebit: {
+    ios: null,
+    android:
+      "https://play.google.com/store/apps/details?id=vn.eplus.smeplatform",
+  },
+  netvas: {
+    ios: "https://apps.apple.com/us/app/netvas-company/id1523015148",
+    android:
+      "https://play.google.com/store/apps/details?id=com.netvas.technical",
+  },
+  mydas: {
+    ios: null,
+    android: "https://play.google.com/store/apps/details?id=com.eplus.mydas",
+  },
+  sale68: {
+    ios: null,
+    android: "https://play.google.com/store/apps/details?id=com.eplus.sales68",
+  },
+  travelaki: {
+    ios: null,
+    android: "https://play.google.com/store/apps/details?id=com.eplus.travelaki.flybooking",
+  },
+  chongluadao: {
+    ios: null,
+    android: "https://play.google.com/store/apps/details?id=vn.eplus.chongluadao",
+  },
+};
 
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   data() {
     return {
       data: data,
       android: null,
       ios: null,
       isLoading: false,
-    }
+    };
   },
   created() {
-    const params = new URLSearchParams(window.location.search)
-    if (params.has('app')) {
-      const dataQuery = params.get('app')
-      const data = this.data
+    const params = new URLSearchParams(window.location.search);
+    if (params.has("app")) {
+      const dataQuery = params.get("app");
+      const data = this.data;
       if (data.hasOwnProperty(`${dataQuery}`) === true) {
-        this.ios = data[dataQuery].ios
-        this.android = data[dataQuery].android
-        this.isLoading = true
+        this.ios = data[dataQuery].ios;
+        this.android = data[dataQuery].android;
+        this.isLoading = true;
       }
     }
   },
   mounted() {
-    const ios = document.getElementById('ios')
-    const android = document.getElementById('android')
+    const ios = document.getElementById("ios");
+    const android = document.getElementById("android");
     if (ios) {
-      ios.addEventListener('click', function () {
+      ios.addEventListener("click", function () {
         if (!ios.href) {
-          alert('Ứng dụng đang cập nhật !!!')
+          alert("Ứng dụng đang cập nhật !!!");
         }
-      })
+      });
     }
     if (android) {
-      android.addEventListener('click', function () {
+      android.addEventListener("click", function () {
         if (!android.href) {
-          alert('Ứng dụng đang cập nhật !!!')
+          alert("Ứng dụng đang cập nhật !!!");
         }
-      })
+      });
     }
   },
-})
+});
